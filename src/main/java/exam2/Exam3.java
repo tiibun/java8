7f84d0eba97b99376bdade09619eef3f386bad58
IIBUN Toshiyuki <toshiyuki.iibun@gmail.com>
2016-08-22 02:03:12 +0900

    Add Files.lines

--- src/main/java/exam2/Exam3.java
+++ src/main/java/exam2/Exam3.java
@@ -1,9 +1,19 @@
 package exam2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Exam3 {
   
   public static void main(String[] args) {
-    // src/main/resources/exam3.txtの内容のタブ区切りの国名の数を求めてください
+    // TODO src/main/resources/exam3.txtの内容のタブ区切りの国名の数を求めてください
+    try (Stream<String> lines = Files.lines(Paths.get("src/main/resources/exam3.txt"))) {
+      
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
